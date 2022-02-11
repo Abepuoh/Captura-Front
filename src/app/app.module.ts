2a87ab4776798650cdd84b915db26edaf653473c
nicorrt <nicorrt123@gmail.com>
2022-02-11 09:31:50 +0100

    nrt-1 first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,20 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule} from '@angular/common/http'
+import { ObraService } from './services/obra.service';
+
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule,
+    HttpClientModule,
+
+    ],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, ObraService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ObraService } from '../services/obra.service';
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +8,34 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  private obras:Array<any>;
 
+  constructor(public obraservice:ObraService) {}
+
+  ngOnInit(){
+    this.obraservice.getAllObras().subscribe(obras=>{
+      this.obras = obras;
+      console.log(this.obras)
+    })
+  }
+
+  public async borra (nota:any){
+    await this.obraservice.deleteObra(nota);
+    console.log(nota);
+  }
+
+  public async getOneObra(id:any){
+    await this.obraservice.getObra(id)
+    console.log(id);
+  }
+
+  public async getObraByName(nombre:any){
+    await this.obraservice.getObraByName(nombre);
+    console.log(nombre);
+  }
+
+  public async getObraByCoordinates(coordenadas:any){
+    await this.obraservice.getObraByCoordinates(coordenadas);
+    console.log(coordenadas);
+  }
 }
