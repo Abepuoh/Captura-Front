7aaa87903ee58f450815da58825883435b9f3202
raultc95 <raultc95@gmail.com>
2022-02-21 17:36:08 +0100

    Editar perfil no aparece en el Login

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -8,13 +9,19 @@ import { Router } from '@angular/router';
 })
 export class LoginPage implements OnInit {
 
-  constructor(private router:Router) { }
+  constructor(private router:Router, public menuCtrl:MenuController) { }
 
   ngOnInit() {
   }
   //Method to navigate to the tabs page
   goToTabsPage(){
     this.router.navigate(['private/tabs/tab1']);
+  }
+  ionViewWillEnter() {
+    this.menuCtrl.enable(false);
+  }
+  ionViewDidLeave() {
+    this.menuCtrl.enable(true);
   }
   
 }
